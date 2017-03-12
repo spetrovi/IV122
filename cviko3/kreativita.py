e8eb1218d78aa520fe8dcb2b083cd72f0d74f2ba
Samuel Petrovic <spetrovi@redhat.com>
2017-03-12 18:37:42 +0100

    obr
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- cviko3/kreativita.py
+++ cviko3/kreativita.py
@@ -1,5 +1,6 @@
 import sys,math
 from PIL import Image, ImageDraw
+from random import randint
 
 class Korytnacka:
    def __init__(self, size,start_position):
@@ -57,7 +58,7 @@ class Korytnacka:
    def save(self,name):
 	self.im.save(name+'.png','png')
 
-def 5star():
+def star():
 	g = Korytnacka(500, (250,250))
 
 	angle = 10
@@ -75,53 +76,30 @@ def 5star():
 		g.forward(forward)
 	g.save('test/hokus_'+str(angle)+'_'+str(angle2)+'_'+str(kvocient)+'_'+str(kvocient2))
 
+def loghokus():
+	g = Korytnacka(500, (250,250))
+
+	a = randint(0,10)
+	forward = math.log10(a)
+	angle = randint(0,90)
+	print a, angle
+	for i in range(0,1000):
+		g.left(angle)
+		g.forward(math.log10(forward**i))
+	g.save('test/loghokus_'+str(a)+'_'+str(angle))
+
+
 
 g = Korytnacka(500, (250,250))
 
-
-
+a = randint(0,10)
+forward = a
+angle = randint(0,90)
+print a, angle
 for i in range(0,1000):
-	g.left(angle)
+	g.left(math.tan(math.radians(angle+10*i)))
 	g.forward(forward)
-	g.right(angle2+i*10)
-	g.back(forward*kvocient2)
-	g.right(angle2+i*10)
-	g.forward(forward)
-g.save('test/hokus2)
-
-#21 92 5.73388719272 0.0
-#17 17 81.8640773224 81.8640773224
-#2 167 3.90874363068 27.3612054148
-#10 19 2.604722665 10.41889066
-#10 45 23.0952076297 207.856868667
-#14 42 3.628828434 18.14414217
-#17 52 4.38557557084 21.9278778542 15
-#31, 14, 16
-#11, 71, 12, 5
-#10 172 65 2
-#podobne cisla, rozdielne vysledky
-#2 104 70 7
-#2 101 66 8
-#5 171 41 * vytvara retaz
-"""
-
-  547  mv hokus13_50_95.png toin/
-  548  mv hokus2_175_68.png toin/
-  561  mv hokus79_86_93_23.png ./toin/hokus2_79_86_93_23.png
-  562  mv hokus25_67_7_9.png ./toin/hokus2_25_67_7_9.png
-  576  mv hokus2_11_71_12_5.png toin/
-  587  mv hokus2_10_172_65_2.png 
-  588  mv hokus2_10_172_65_2.png toin/
-  632  mv hokus3_2_101_66_8.png toin/
-  633  mv hokus3_2_104_70_7.png toin/
-  649  mv hokus4_7_175_70_2.png toin/
-"""
-
-
-
-
-
-
+g.save('test/radhokus_'+str(a)+'_'+str(angle))
 
 
 
