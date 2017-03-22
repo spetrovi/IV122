7cbf3c3b857c9daff3cdb9b67631a9e212745d41
Samuel Petrovic <spetrovi@redhat.com>
2017-03-22 10:19:33 +0100

    Konvexny obal dokonceny
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- cviko5/konvex.py
+++ cviko5/konvex.py
@@ -1,18 +1,7 @@
-from __future__ import division
 import math
 from random import choice
 from PIL import Image, ImageDraw
 
-
-def draw_p(points):
-	size = 500
-	im = Image.new('L', (size,size),255)
-	draw = ImageDraw.Draw(im) 
-	for point in points:
-		for x in range(point[0]-2,point[0]+2):
-			for y in range(point[1]-2,point[1]+2):
-				im.putpixel((x,y),0)
-	im.save('konvexP.png','png')
 
 def draw_it(points, edge):
 	size = 500
@@ -24,79 +13,46 @@ def draw_it(points, edge):
 				im.putpixel((x,y),0)
 
 	for i in range(0,len(edge)-1):
-		draw.line((edge[i][0],edge[i][1],edge[i+1][0],edge[i+1][1]),fill=0)
+		draw.line((edge[i][0],edge[i][1],edge[i+1][0],edge[i+1][1]),fill = 0)
+	draw.line((edge[i+1][0],edge[i+1][1],edge[0][0],edge[0][1]), fill = 0)
 	im.save('konvex.png','png')
 		
 def generate_points(n):
 	size = 400
 	points = []
 	for i in range(0,n):
-		x = choice(range(size))
-		y = choice(range(size))
+		x = choice(range(100,size))
+		y = choice(range(100,size))
 		points.append((x,y))
 	return points
 
 def get_x((x,y)):
 	return x
 
-def generate_sorted_lines(points):
-	lines = map(lambda x: tuple(x), combinations(points,2))
-	lines.sort(key=get_length)
-	return lines
+#(C is on left of line from A to B)
+def left((xA,yA),(x1,y1),(x2,y2)):
+	v1 = (x2-x1, y2-y1)   # Vector 1
+	v2 = (x2-xA, y2-yA)   # Vector 1
+	xp = v1[0]*v2[1] - v1[1]*v2[0]  # Cross product
+	if xp > 0:
+    		return True
+	return False
 
-#ked y bodu 2 je mensia ako y bodu 1: x/y
-#ked ybodu 2 je vacsia ako y bodu 1: y/x
-#math.degrees(math.atan(abs(point[1]-y)/abs(point[0]-x)))+90
- # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
- # Returns a positive value, if OAB makes a counter-clockwise turn,
-def cross(o, a, b):
-	return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
-def nice_point((x,y), points):
-	points.remove((x,y))
-	good_point = points[0]
-	c = cross((x,y), (x,y+10), good_point)
-	#if (good_point[1] > y and good_point[0] > x) or (good_point[1] < y and good_point[0] < x):
-	#	c = math.degrees(math.atan(abs(good_point[0]-x)/abs(good_point[1]-y)))
-	#else:
-	#	c = math.degrees(math.atan(abs(good_point[1]-y)/abs(good_point[0]-x)))+90
-	print 'kokotpica'	
-	#print (x,y)
-	for point in points:
-		print point
-		print cross((x,y), (x,y+10), point)
-		if cross((x,y), (x,y+10), point) < 0 and cross((x,y), (x,y+10), point) > c:
-		#if (point[1] > y and point[0] > x) or (point[1] < y and point[0] < x):
-		#	angle = math.degrees(math.atan(abs(point[0]-x)/abs(point[1]-y)))
-		#else:
-		#	angle = math.degrees(math.atan(abs(point[1]-y)/abs(point[0]-x)))+90
-		#print angle
-		#if angle < c:
-			good_point = point
-			c = cross((x,y), (x,y+10), point)
-		#	c = angle
-
-	print good_point
-	return good_point
-
-points = generate_points(10)
+points = generate_points(100)
 points.sort(key=lambda (x,y): x)
 
-points = [(36, 244), (77, 344), (143, 257), (250, 164), (300, 400)]
 td = points[:]
-points_on_edge = [points[0]]
-current_point = points[0]
-#points.append(current_point)
-#first_point = current_point
-#points.remove(first_point)
-#print points
-draw_p(points)
+point_on_hull = points[0]
+endpoint = points[1]
+points_on_edge = []
 
-for i in range(0,4):
-	#points.remove(current_point)
-	current_point = nice_point(current_point, points[:])
-	points_on_edge.append(current_point)
-	print points
-
+while endpoint != points[0]:
+	points_on_edge.append(point_on_hull)
+	endpoint = points[0]
+	for point in points[1:]:
+		if endpoint == point_on_hull or left(point, point_on_hull, endpoint):
+			endpoint = point
+	point_on_hull = endpoint
 print points_on_edge
 draw_it(td, points_on_edge)
 	
