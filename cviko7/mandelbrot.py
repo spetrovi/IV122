c59f11a448b83d1d5715690d52cfc7d839ade35f
Samuel Petrovic <spetrovi@redhat.com>
2017-04-04 15:37:56 +0200

    zoom
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- cviko7/mandelbrot.py
+++ cviko7/mandelbrot.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw 
 import math
 
-size = 1000
+size = 600
 
 im = Image.new('RGB', (size, size))
 
-x = np.linspace(-2.2,1, size, endpoint=False)
-y = np.linspace(-1.6, 1.6, size, endpoint=False)
+x = (-2,1)
+y = (-1.5,1.5)
+
+x = np.linspace(x[0],x[1], size, endpoint=False)
+y = np.linspace(y[0],y[1], size, endpoint=False)
 
 for cx, i in enumerate(x):
 	for cy, j in enumerate(y):
@@ -22,15 +25,10 @@ for cx, i in enumerate(x):
 		if abs(z) < 2:			#is mandel
 			im.putpixel((cx,cy),(0,0,0)) 
 		else: 				#is not mandel
-			b = int(sum(v)/len(v))
-#vzdialenost kvadrant do ktoreho sa postupnost dostala a priemerna vzdialenost od nuly
-			if z.real > 0 and z.imag > 0: im.putpixel((cx,cy),(b,0,0)) 
-			if z.real > 0 and z.imag < 0: im.putpixel((cx,cy),(b,0,b)) 
-			if z.real < 0 and z.imag > 0: im.putpixel((cx,cy),(0,0,b)) 
-			if z.real < 0 and z.imag < 0: im.putpixel((cx,cy),(0,b,0))		
+			b = int(sum(v)/len(v))# priemerna vzdialenost od nuly
 
 #kolko iteracii trvalo, nez postupnost opustila 2
-#			im.putpixel((cx,cy),(k**2,k**2,k**2))
+			im.putpixel((cx,cy),(k**2,k**2,k**2))
 
 # do ktoreho kvadrantu sa postuponst dostala
 #			if z.real > 0 and z.imag > 0: im.putpixel((cx,cy),(255,0,0)) 
