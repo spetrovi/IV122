436cd09d2de9c8b8a333d46e4e9c7cdaf603a7f7
Samuel Petrovic <spetrovi@redhat.com>
2017-04-11 16:44:48 +0200

    con
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- cviko8/1.py
+++ cviko8/1.py
@@ -33,7 +33,7 @@ def combine(ops):
 	return reduce(lambda x,y: matrix_multi(x,y), list(reversed(ops)), [[1,0,0],[0,1,0],[0,0,1]])
 
 def demo(it, transform, obj):
-	size = 1000
+	size = 500
 	im = Image.new('RGB', (size,size), (255,255,255))
 	draw = ImageDraw.Draw(im)
 	map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),obj)
@@ -41,19 +41,15 @@ def demo(it, transform, obj):
 		for i,line in enumerate(obj):
 			point1 = matrix_multi(transform,[[line[0]],[line[1]],[1]])
 			point2 = matrix_multi(transform,[[line[2]],[line[3]],[1]])
-			print 'points'
-			print point1
-			print point2
 			obj[i] = [point1[0][0],point1[1][0],point2[0][0],point2[1][0]]
-		for lin in obj:
-			#print 'drawing...'
-			#print (lin[0]+size/2,lin[1]+size/2,lin[2]+size/2,lin[3]+size/2)
-			draw.line((lin[0]+size/2,lin[1]+size/2,lin[2]+size/2,lin[3]+size/2),fill=0)
-		#map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),obj)
+		map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),obj)
 	im.save('demo.png','png')
 
-#demo(3, combine([shear(1.3), rotate(10), scale(0.9,0.9), translate(50, 50)]), square(10))
+#demo(25, combine([shear(1.3), rotate(10), scale(0.9,0.9), translate(5, 5)]), square(20))
+#demo(10, combine([rotate(20), scale(1.1, 1.1), translate(5, 10)]), square(20))
 
+demo(15, combine([rotate(10), scale(1.1, 0.8)]), square(100))
+"""
 sq = square(50)
 size = 1000
 im = Image.new('RGB', (size,size), (255,255,255))
@@ -61,8 +57,10 @@ draw = ImageDraw.Draw(im)
 
 map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),sq)
 transform = combine([shear(1.3), rotate(10), scale(0.9,0.9), translate(50, 50)])
-#transform = rotate(360)
+transform = rotate(30)
 #transform = translate(10,10)
+#transform = shear(1.3)
+#transform = scale(0.5,0.5)
 print transform
 for i,line in enumerate(sq):
 	point1 = matrix_multi(transform,[[line[0]],[line[1]],[1]])
@@ -70,14 +68,14 @@ for i,line in enumerate(sq):
 	sq[i] = [point1[0][0],point1[1][0],point2[0][0],point2[1][0]]
 map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),sq)
 
-for i,line in enumerate(sq):
-	point1 = matrix_multi(transform,[[line[0]],[line[1]],[1]])
-	point2 = matrix_multi(transform,[[line[2]],[line[3]],[1]])
-	sq[i] = [point1[0][0],point1[1][0],point2[0][0],point2[1][0]]
-map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),sq)
+#for i,line in enumerate(sq):
+#	point1 = matrix_multi(transform,[[line[0]],[line[1]],[1]])
+#	point2 = matrix_multi(transform,[[line[2]],[line[3]],[1]])
+#	sq[i] = [point1[0][0],point1[1][0],point2[0][0],point2[1][0]]
+#map(lambda (x1,x2,y1,y2): draw.line((x1+size/2,x2+size/2,y1+size/2,y2+size/2),fill=0),sq)
 im.save('demo.png','png')
 
-
+"""
 
 
 
