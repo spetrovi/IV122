a842d4173aff7d53f926e2f1aa9f09efa2b9758f
Samuel Petrovic <spetrovi@redhat.com>
2017-05-30 13:16:08 +0200

    sequence 4
    Signed-off-by: Samuel Petrovic <spetrovi@redhat.com>

--- cviko9/nah.py
+++ cviko9/nah.py
@@ -1,13 +1,6 @@
 from random import choice 
 import numpy as np
 
-#print 'Sequence 2'
-f = open('random2.txt','r')
-s2 = f.read().split(' ')[:-1]
-f.close()
-s2 = map(lambda x: int(x), s2)
-print np.histogram(s2, bins=[1,2,3,4,5,6,7])
-
 print 'Sequence1'
 f = open('random1.txt','r')
 s1 = f.read().split(' ')[:-1]
@@ -23,5 +16,18 @@ for i, num in enumerate(s1[:-1]):
 for stat in stats:
 	print np.histogram(stat, bins=[1,2,3,4,5,6,7])
 
+#print 'Sequence 2'
+f = open('random2.txt','r')
+s2 = f.read().split(' ')[:-1]
+f.close()
+s2 = map(lambda x: int(x), s2)
+#print np.histogram(s2, bins=[1,2,3,4,5,6,7]
 
 
+print 'Sequence4'
+f = open('random4.txt','r')
+s1 = f.read().split(' ')[:-1]
+s1 = map(lambda x: int(x), s1)
+f.close()
+
+stats = [[] for i in range(6)]
